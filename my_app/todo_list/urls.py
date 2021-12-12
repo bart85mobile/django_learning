ec620aa003866063a310362acc1836781beec59d
Bart Cybulski <bart85mobile@yandex.com>
2021-12-12 14:21:19 +0100

    add_urls_file

--- my_app/todo_list/urls.py
+++ my_app/todo_list/urls.py
@@ -1,4 +1,4 @@
-"""todo_app URL Configuration
+"""todo_list URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from todo_list import views
+
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo_list.urls')),
+
 ]
